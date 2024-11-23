4a3d3d224cd17cd060acb8a65f2c84b47f23aa56
Kalisia Andrade <kalisia.autuori@gmail.com>
2024-11-23 17:57:37 +0000

    change somethings again

---
+++ cypress/e2e/signup-error.cy.js
@@ -0,0 +1,11 @@
+describe('sigup-error', () => {
+    it('sigup-error', () => {
+      cy.visit('https://adopet-tau.vercel.app/');
+      cy.get('[href="/cadastro"]').click();
+      cy.get('button').click();
+      cy.contains('É necessário informar seu nome').should('be.visible');
+      cy.contains('É necessário informar um endereço de email').should('be.visible');
+      cy.contains('Crie uma senha').should('be.visible');
+      cy.contains('Repita a senha criada acima').should('be.visible');
+    })
+  })
